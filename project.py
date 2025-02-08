c2b65971aa67b822b6ecab0fb7f4bf0ff06125ff
Daksh-Singh5 <dakshsingh6612@gmail.com>
2025-02-08 07:53:33 +0530

    awda

---
+++ project.py
@@ -0,0 +1,15 @@
+Frequincy = {
+    "1":"3",
+    "2":"4",
+    "3":"21",
+    "4":"12",
+    "5":"41",
+    "6":"20",
+    "7":"13",
+    "8":"3",
+    "9":"14",
+    "0":"1",
+}
+
+number = input("Which Number do you want: ")
+print("The ",number+"'s","frequincy is:",Frequincy.get(number,"This is not give"))
